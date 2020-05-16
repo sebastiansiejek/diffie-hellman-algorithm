8c4f0f6df598f7f8328f4519ec5265156d581dc1
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-05-16 14:57:52 +0200

    feat: add algorithm

--- DH.py
+++ DH.py
@@ -3,6 +3,26 @@ from utils.generatePrime import generatePrimeNumber
 
 class DH:
     n = 0
+    g = 0
+    privateKey = 0
+    sharedKey = 0
+    foreignKey = 0
+    sessionKey = 0
 
-    def __init__(self):
-        self.n = generatePrimeNumber()
+    def __init__(self, n, g):
+        self.n = n
+        self.g = g
+
+    def setPrivateKey(self, x):
+        self.privateKey = x
+        self.sharedKey = pow(self.g, x) % self.n
+        return self
+
+    def setForeignKey(self, key):
+        self.foreignKey = key
+        self.setSessionKey()
+        return self
+
+    def setSessionKey(self):
+        self.sessionKey = pow(self.foreignKey, self.privateKey) % self.n
+        return self
