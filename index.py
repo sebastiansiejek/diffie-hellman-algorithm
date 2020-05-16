42cfa6210bd558373e152bcfc1f568dae2de578a
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-05-16 14:58:30 +0200

    feat: add sample usage

--- index.py
+++ index.py
@@ -1,3 +1,10 @@
 from DH import DH
 
-DH()
+a = DH(23, 5).setPrivateKey(6)
+b = DH(23, 5).setPrivateKey(15)
+
+a.setForeignKey(b.sharedKey)
+b.setForeignKey(a.sharedKey)
+
+print(a.sessionKey)
+print(b.sessionKey)
